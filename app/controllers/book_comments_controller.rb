e14e4d06ef5a66e7a0b542b0fac80745fee091f2
ayano-w <ayasaku.t5@gmail.com>
2021-11-09 07:57:45 +0000

    [Add]add the function to count comments.

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,11 +2,14 @@ class BookCommentsController < ApplicationController
     
     def create
         @book = Book.find(params[:book_id])
-        comment = current_user.book_comments.new(book_comment_params)
-        comment.book_id = @book.id
-        comment.save
-        @book_comments = @book.book_comments
-        @book_comment = BookComment.new
+        @book_comment = current_user.book_comments.new(book_comment_params)
+        @book_comment.book_id = @book.id
+        if @book_comment.save
+         @book_comments = @book.book_comments
+        else
+            render 'layouts/error'
+        end
+        
        
     end
     
